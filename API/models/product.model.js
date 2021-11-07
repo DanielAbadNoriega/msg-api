fb7b22bd5863fa3f930e048400bf357bcd41c370
Daniel Abad Noriega <Daniel.Abad.Noriega@gmail.com>
2021-11-07 17:37:50 +0100

    Iteration: V1.Seed

--- API/models/product.model.js
+++ API/models/product.model.js
@@ -1,39 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema ({
+const productSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: 'Name is required'
+      type: String,
+      required: "Name is required",
     },
     nutritionPerHundred: {
-        type: {String},
+      type: "object",
     },
     image: {
-        type: String
+      type: String,
     },
     tags: {
-        type: [String]
-    }
-}, {
+      type: String,
+    },
+  },
+  {
     timestamps: true,
     toJSON: {
-        virtuals: true,
-        transform: (doc, ret) => {
-            ret.id = doc._id;
-            delete ret._id;
-            delete ret.__v;
-            return ret;
-        }
-    }
-})
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.id = doc._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
 
-productSchema.virtual('rating', {
-    ref: 'Rating',
-    localField: '_id',
-    foreignField: 'product',
-    justOne: false
-})
+productSchema.virtual("rating", {
+  ref: "Rating",
+  localField: "_id",
+  foreignField: "product",
+  justOne: false,
+});
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
